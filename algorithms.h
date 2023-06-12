56aa99b71b9bc2b75744cd92d3f00cd5d630b7ab
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2023-06-12 22:46:24 +0000

    Create algorithms.h

---
+++ algorithms.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+#include <queue>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::make_pair;
+using std::min;
+using std::max;
+using std::pair;
+using std::priority_queue;
+using std::set;
+using std::vector;
+
+const int kInf = 100000;
+const int startVertex = 0;
+
+int finishVertex; // = n - 1
+int answer;
+
+struct Edge {
+    int a, b, weight;
+};
+
+int isPath(int ans);
+int Dijkstra(int n, int m, vector<vector<int>>& data);
+int FloydWarshall(int n, int m, vector<vector<int>>& data);
+int BellmanFord(int n, int m, vector<vector<int>>& data);
+int AStar(int n, int m, vector<vector<int>>& data);
